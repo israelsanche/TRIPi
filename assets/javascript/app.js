68d52314f7d6df0ce0344e202b3e98ffe14dbab3
Tyler Larrabee <tyler.k.larrabee@gmail.com>
2019-05-01 18:09:17 -0700

    Load in cards for Yelp, Zomato, Yelp Hotel with DOM. Zomato API up and running.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
 // Global Variables
 
 var locationQuery = "";
-
-
+var zomatoQuery = "";
+$("#yelpDiv").hide();
+$("#zomatoDiv").hide();
+$("#hotelDiv").hide();
 mapboxgl.accessToken = 'pk.eyJ1IjoidHlsZXItbGFycmFiZWUiLCJhIjoiY2p1dnU1bjh5MDVrNDQ0bGoyNWtreWRnZiJ9.eF1RC1zYSNU6iDVUE2FIqw';
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/dark-v10'
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10'
 });
 
 var geocoder = new MapboxGeocoder({
@@ -18,21 +20,22 @@ $("#citytext").append(geocoder.onAdd(map));
 
 // geocoder.query("New York");
 geocoder.setPlaceholder("Set Location");
-geocoder.on("result", function(e){
+geocoder.on("result", function (e) {
     console.log(e.result);
-locationQuery = e.result.place_name;
-console.log(locationQuery);
+    locationQuery = e.result.place_name;
+    zomatoQuery = e.result.geometry.coordinates;
+    console.log(zomatoQuery);
 })
 
 
 $("#city-click").on("click", function (event) {
-   
+
     console.log($("#city-search").val());
     event.preventDefault();
-    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=" + locationQuery;
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&location=" + locationQuery;
     var hotelurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=hotels&location=" + locationQuery;
     var eventurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=events&location=" + locationQuery;
-var weatherurl = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location=" + locationQuery;
+    var weatherurl = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location=" + locationQuery;
 
     $.ajax({
         url: myurl,
@@ -42,38 +45,26 @@ var weatherurl = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location="
         method: 'GET',
         dataType: 'json',
         success: function (response) {
-            console.log(response);
-            var address1 = response.businesses[0].location.address1 + ", " + response.businesses[0].location.city + ", " + response.businesses[0].location.state + " " + response.businesses[0].location.zip_code;
-            var address2 = response.businesses[1].location.address1 + ", " + response.businesses[1].location.city + ", " + response.businesses[1].location.state + " " + response.businesses[1].location.zip_code;
-            var address3 = response.businesses[2].location.address1 + ", " + response.businesses[2].location.city + ", " + response.businesses[2].location.state + " " + response.businesses[2].location.zip_code;
-            var address4 = response.businesses[3].location.address1 + ", " + response.businesses[3].location.city + ", " + response.businesses[3].location.state + " " + response.businesses[3].location.zip_code;
-            var address5 = response.businesses[4].location.address1 + ", " + response.businesses[4].location.city + ", " + response.businesses[4].location.state + " " + response.businesses[4].location.zip_code;
 
-            $("#businessName1").text(response.businesses[0].name);
-            $("#phoneNumber1").text(response.businesses[0].phone);
-            $("#address1").text(address1);
-            $("#rating1").text("Business is Rated: " + response.businesses[0].rating);
-
-            $("#businessName2").text(response.businesses[1].name);
-            $("#phoneNumber2").text(response.businesses[1].phone);
-            $("#address2").text(address2);
-            $("#rating2").text("Business is Rated: " + response.businesses[1].rating);
-
-            $("#businessName3").text(response.businesses[2].name);
-            $("#phoneNumber3").text(response.businesses[2].phone);
-            $("#address3").text(address3);
-            $("#rating3").text("Business is Rated: " + response.businesses[2].rating);
-
-            $("#businessName4").text(response.businesses[3].name);
-            $("#phoneNumber4").text(response.businesses[3].phone);
-            $("#address4").text(address4);
-            $("#rating4").text("Business is Rated: " + response.businesses[3].rating);
-
-            $("#businessName5").text(response.businesses[4].name);
-            $("#phoneNumber5").text(response.businesses[4].phone);
-            $("#address5").text(address5);
-            $("#rating5").text("Business is Rated: " + response.businesses[4].rating);
-
+            if (response.businesses.length > 0) {
+                $("#yelpDiv").show();
+                $("#yelp").empty();
+                for (i = 0; i < 10; i++) {
+                    var card = $("<div class='card'>");
+                    var cardBody = $("<div class='card-body'>");
+                    var businessTitle = $("<h4 class='card-title'>").text(response.businesses[i].name);
+                    var phone = $("<p class='card-subtitle mb-2 text-muted'>").text(response.businesses[i].phone);
+                    var address = $("<p class='card-text'>").text(response.businesses[i].location.address1 + ", " + response.businesses[i].location.city + ", " + response.businesses[i].location.state + " " + response.businesses[i].location.zip_code);
+                    var rating = $("<p class='card-text'>").text("Business is Rated: " + response.businesses[i].rating);
+                    $(card).append(cardBody);
+                    $(cardBody).append(businessTitle);
+                    $(cardBody).append(phone);
+                    $(cardBody).append(address);
+                    $(cardBody).append(rating);
+                    $(card).css("width", "20%");
+                    $("#yelp").append(card);
+                }
+            }
         }
     });
     $.ajax({
@@ -84,76 +75,66 @@ var weatherurl = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location="
         method: 'GET',
         dataType: 'json',
         success: function (response2) {
-            console.log(response2);
-            var address12 = response2.businesses[0].location.address1 + ", " + response2.businesses[0].location.city + ", " + response2.businesses[0].location.state + " " + response2.businesses[0].location.zip_code;
-            var address22 = response2.businesses[1].location.address1 + ", " + response2.businesses[1].location.city + ", " + response2.businesses[1].location.state + " " + response2.businesses[1].location.zip_code;
-            var address32 = response2.businesses[2].location.address1 + ", " + response2.businesses[2].location.city + ", " + response2.businesses[2].location.state + " " + response2.businesses[2].location.zip_code;
-            var address42 = response2.businesses[3].location.address1 + ", " + response2.businesses[3].location.city + ", " + response2.businesses[3].location.state + " " + response2.businesses[3].location.zip_code;
-            var address52 = response2.businesses[4].location.address1 + ", " + response2.businesses[4].location.city + ", " + response2.businesses[4].location.state + " " + response2.businesses[4].location.zip_code;
 
-            $("#2businessName1").text(response2.businesses[0].name);
-            $("#2phoneNumber1").text(response2.businesses[0].phone);
-            $("#2address1").text(address12);
-            $("#2rating1").text("Business is Rated: " + response2.businesses[0].rating);
-
-            $("#2businessName2").text(response2.businesses[1].name);
-            $("#2phoneNumber2").text(response2.businesses[1].phone);
-            $("#2address2").text(address22);
-            $("#2rating2").text("Business is Rated: " + response2.businesses[1].rating);
-
-            $("#2businessName3").text(response2.businesses[2].name);
-            $("#2phoneNumber3").text(response2.businesses[2].phone);
-            $("#2address3").text(address32);
-            $("#2rating3").text("Business is Rated: " + response2.businesses[2].rating);
-
-            $("#2businessName4").text(response2.businesses[3].name);
-            $("#2phoneNumber4").text(response2.businesses[3].phone);
-            $("#2address4").text(address42);
-            $("#2rating4").text("Business is Rated: " + response2.businesses[3].rating);
-
-            $("#2businessName5").text(response2.businesses[4].name);
-            $("#2phoneNumber5").text(response2.businesses[4].phone);
-            $("#2address5").text(address52);
-            $("#2rating5").text("Business is Rated: " + response2.businesses[4].rating);
-
+            if (response2.businesses.length > 0) {
+                $("#hotelDiv").show();
+                $("#yelpHotel").empty();
+                for (i = 0; i < 10; i++) {
+                    var card = $("<div class='card'>");
+                    var cardBody = $("<div class='card-body'>");
+                    var businessTitle = $("<h4 class='card-title'>").text(response2.businesses[i].name);
+                    var phone = $("<p class='card-subtitle mb-2 text-muted'>").text(response2.businesses[i].phone);
+                    var address = $("<p class='card-text'>").text(response2.businesses[i].location.address1 + ", " + response2.businesses[i].location.city + ", " + response2.businesses[i].location.state + " " + response2.businesses[i].location.zip_code);
+                    var rating = $("<p class='card-text'>").text("Business is Rated: " + response2.businesses[i].rating);
+                    $(card).append(cardBody);
+                    $(cardBody).append(businessTitle);
+                    $(cardBody).append(phone);
+                    $(cardBody).append(address);
+                    $(cardBody).append(rating);
+                    $(card).css("width", "20%");
+                    $("#yelpHotel").append(card);
+                }
+            }
         }
     });
 
 
-  
+
     var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
     // Here we are building the URL we need to query the database
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q="+ locationQuery + "&units=imperial&appid=" + APIKey;
+        "q=" + locationQuery + "&units=imperial&appid=" + APIKey;
 
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
-      url: queryURL,
-      method: "GET"
+        url: queryURL,
+        method: "GET"
     })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(weatherresponse) {
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function (weatherresponse) {
 
-        // Log the queryURL
-        console.log(queryURL);
+            // Log the queryURL
+            console.log(queryURL);
 
-        // Log the resulting object
-        console.log(weatherresponse);
+            // Log the resulting object
+            console.log(weatherresponse);
 
-        // code to display weather on html here...
+            // code to display weather on html here...
 
 
-      });
-      //ZOMATO API//
-//   api key: d26b1d8dcb1755149c3453df8f881735 //
+        });
+    //ZOMATO API//
+    //   api key: d26b1d8dcb1755149c3453df8f881735 //
 
-// click function that's gonna trigger that call
+    // click function that's gonna trigger that call
 
     event.preventDefault();
     //grabbing value of the search input//
+    // Change to a geocode request and pass lon lat to the call and call the list of restaurants in the area. 
     console.log("dude you're searching it up!");
-    var zomatourl = "https://developers.zomato.com/api/v2.1/search?entity_type=city" + locationQuery + "&apikey=d26b1d8dcb1755149c3453df8f881735"
+    // var zomatourl = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=" + zomatoQuery + "&apikey=d26b1d8dcb1755149c3453df8f881735"
+    var zomatourl = "https://developers.zomato.com/api/v2.1/geocode?lat=" + zomatoQuery[1] + "&lon=" + zomatoQuery[0] + "&apikey=d26b1d8dcb1755149c3453df8f881735"
     $.ajax({
         url: zomatourl,
         method: "GET",
@@ -161,33 +142,26 @@ var weatherurl = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location="
     }).then(function (zomatoresponse) {
         console.log(zomatoresponse);
         // code to display to html here...
-
-            $("#zomatobusinessName1").text(zomatoresponse.restaurants[0].restaurant.name);
-            // $("#zomatophoneNumber1").text(zomatoresponse.restaurants[0].restaurant.phone);
-            $("#zomatoaddress1").text(zomatoresponse.restaurants[0].restaurant.location.address);
-            $("#zomatorating1").text("Business is Rated: " + zomatoresponse.restaurants[0].restaurant.user_rating.aggregate_rating);
-
-            $("#zomatobusinessName2").text(zomatoresponse.restaurants[1].restaurant.name);
-            // $("#zomatophoneNumber2").text(zomatoresponse.businesses[1].phone);
-            $("#zomatoaddress2").text(zomatoresponse.restaurants[1].restaurant.location.address);
-            $("#zomatorating2").text("Business is Rated: " + zomatoresponse.restaurants[1].restaurant.user_rating.aggregate_rating);
-
-            $("#zomatobusinessName3").text(zomatoresponse.restaurants[2].restaurant.name);
-            // $("#zomatophoneNumber3").text(zomatoresponse.businesses[2].phone);
-            $("#zomatoaddress3").text(zomatoresponse.restaurants[2].restaurant.location.address);
-            $("#zomatorating3").text("Business is Rated: " + zomatoresponse.restaurants[2].restaurant.user_rating.aggregate_rating);
-
-            $("#zomatobusinessName4").text(zomatoresponse.restaurants[3].restaurant.name);
-            // $("#zomatophoneNumber4").text(zomatoresponse.businesses[3].phone);
-            $("#zomatoaddress4").text(zomatoresponse.restaurants[3].restaurant.location.address);
-            $("#zomatorating4").text("Business is Rated: " + zomatoresponse.restaurants[3].restaurant.user_rating.aggregate_rating);
-
-            $("#zomatobusinessName5").text(zomatoresponse.restaurants[4].restaurant.name);
-            // $("#zomatophoneNumber5").text(zomatoresponse.businesses[4].phone);
-            $("#zomatoaddress5").text(zomatoresponse.restaurants[4].restaurant.location.address);
-            $("#zomatorating5").text("Business is Rated: " + zomatoresponse.restaurants[4].restaurant.user_rating.aggregate_rating);
-
-    })
+        if (zomatoresponse.nearby_restaurants.length > 0) {
+            $("#zomatoDiv").show();
+            $("#zomato").empty();
+            for (i = 0; i < 10; i++) {
+                var card = $("<div class='card'>");
+                var cardBody = $("<div class='card-body'>");
+                var businessTitle = $("<h4 class='card-title'>").text(zomatoresponse.nearby_restaurants[i].restaurant.name);
+                var phone = $("<p class='card-subtitle mb-2 text-muted'>").text(zomatoresponse.nearby_restaurants[i].restaurant.phone);
+                var address = $("<p class='card-text'>").text(zomatoresponse.nearby_restaurants[i].restaurant.location.address);
+                var rating = $("<p class='card-text'>").text("Business is Rated: " + zomatoresponse.nearby_restaurants[i].restaurant.user_rating.aggregate_rating);
+                $(card).append(cardBody);
+                $(cardBody).append(businessTitle);
+                $(cardBody).append(phone);
+                $(cardBody).append(address);
+                $(cardBody).append(rating);
+                $(card).css("width", "20%");
+                $("#zomato").append(card);
+            }
+        }
+    });
 
 
 
