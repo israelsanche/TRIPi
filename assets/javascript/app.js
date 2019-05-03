28dc4a9642df8dc90f6c0ca3db5701b2ad5e2cd8
Tyler Larrabee <tyler.k.larrabee@gmail.com>
2019-05-03 12:26:20 -0700

    cleaned up code, removed console logs for yelp, zomato, and mapbox. Geolocate Added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
-// Global Variables
 $(document).ready(function () {
 
     var locationQuery = "";
-    var zomatoQuery = "";
+    var geoQuery = "";
     var markerHeight = 50, markerRadius = 10, linearOffset = 25;
     var popupOffsets = {
         'top': [0, 0],
@@ -19,10 +18,12 @@ $(document).ready(function () {
     $("#zomatoDiv").hide();
     $("#hotelDiv").hide();
     $("#eventsDiv").hide();
+
     mapboxgl.accessToken = 'pk.eyJ1IjoidHlsZXItbGFycmFiZWUiLCJhIjoiY2p1dnU1bjh5MDVrNDQ0bGoyNWtreWRnZiJ9.eF1RC1zYSNU6iDVUE2FIqw';
     var map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/dark-v10'
+        style: 'mapbox://styles/mapbox/dark-v10',
+        zoom: 3
     });
 
     var geocoder = new MapboxGeocoder({
@@ -30,171 +31,32 @@ $(document).ready(function () {
         mapboxgl: mapboxgl
     });
 
+    var locator = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    });
+
+    map.addControl(locator)
+
     $("#citytext").append(geocoder.onAdd(map));
 
     // geocoder.query("New York");
     geocoder.setPlaceholder("Set Location");
 
     geocoder.on("result", function (e) {
-        console.log(e.result);
-        locationQuery = e.result.place_name;
-        zomatoQuery = e.result.geometry.coordinates;
-        console.log(zomatoQuery);
-
-
-        console.log($("#city-search").val());
+        
         event.preventDefault();
-        var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&location=" + locationQuery;
-        var hotelurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=hotels&location=" + locationQuery;
-        var eventurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=events&location=" + locationQuery;
+        locationQuery = e.result.place_name;
+        geoQuery = e.result.geometry.coordinates;
+
+        yelp("restaurant", locationQuery, "#yelp", "#yelpDiv", "red");
+        yelp("hotels", locationQuery, "#yelpHotel", "#hotelDiv", "purple");
+        yelp("events", locationQuery, "#events", "#eventsDiv", "yellow");
+        zomato(geoQuery);
+
         var weatherurl = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location=" + locationQuery;
-
-
-        $.ajax({
-            url: myurl,
-            headers: {
-                'Authorization': 'Bearer BMqPeh9LSaDcy2Ynzrlg9b3Pg-zqx7NkPeU6d042djt_xHyFXUHMMftzKLTkw_ftN-_m3GRnBIxeyfvo_U9wHH1WRcYx0LsH6TMMMtyyqgp77l_raGrlydIVQUzBXHYx',
-            },
-            method: 'GET',
-            dataType: 'json',
-            success: function (response) {
-                console.log(response)
-                if (response.businesses.length > 0) {
-                    $("#yelpDiv").show();
-                    $("#yelp").empty();
-                    for (i = 0; i < 10; i++) {
-
-
-                        var card = $("<div class='card'>");
-                        var cardBody = $("<div class='card-body'>");
-                        var businessTitle = $("<a class='card-title'>").text(response.businesses[i].name);
-                        businessTitle.attr("href", response.businesses[i].url);
-                        var phone = $("<p class='card-subtitle mb-2 text-muted'>").text(response.businesses[i].phone);
-                        var address = $("<p class='card-text'>").text(response.businesses[i].location.address1 + ", " + response.businesses[i].location.city + ", " + response.businesses[i].location.state + " " + response.businesses[i].location.zip_code);
-                        var rating = $("<p class='card-text'>").text("Rating: " + response.businesses[i].rating);
-                        var price = $("<p class='card-text'>").text("Price: " + response.businesses[i].price);
-
-
-
-                        var popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
-                        popup.setLngLat({ lon: response.businesses[i].coordinates.longitude, lat: response.businesses[i].coordinates.latitude });
-                        popup.setHTML("<p>" + response.businesses[i].name + "</p>")
-                        popup.setMaxWidth("300px")
-                        // popup.addTo(map);
-
-
-
-                        var marker = new mapboxgl.Marker({color: "red" })
-                        marker.setLngLat([response.businesses[i].coordinates.longitude, response.businesses[i].coordinates.latitude])
-                        marker.addTo(map);
-                        marker.setPopup(popup);
-
-
-                        $(card).append(cardBody);
-                        $(cardBody).append(businessTitle);
-                        $(cardBody).append(phone);
-                        $(cardBody).append(address);
-                        $(cardBody).append(rating);
-                        $(cardBody).append(price);
-                        $(card).css("width", "20%");
-                        $("#yelp").append(card);
-                    }
-                }
-            }
-        });
-        $.ajax({
-            url: hotelurl,
-            headers: {
-                'Authorization': 'Bearer BMqPeh9LSaDcy2Ynzrlg9b3Pg-zqx7NkPeU6d042djt_xHyFXUHMMftzKLTkw_ftN-_m3GRnBIxeyfvo_U9wHH1WRcYx0LsH6TMMMtyyqgp77l_raGrlydIVQUzBXHYx',
-            },
-            method: 'GET',
-            dataType: 'json',
-            success: function (response2) {
-                console.log(response2)
-                if (response2.businesses.length > 0) {
-                    $("#hotelDiv").show();
-                    $("#yelpHotel").empty();
-                    for (i = 0; i < 10; i++) {
-                        var card = $("<div class='card'>");
-                        var cardBody = $("<div class='card-body'>");
-                        var businessTitle = $("<a class='card-title'>").text(response2.businesses[i].name);
-                        businessTitle.attr("href", response2.businesses[i].url);
-                        var phone = $("<p class='card-subtitle mb-2 text-muted'>").text(response2.businesses[i].phone);
-                        var address = $("<p class='card-text'>").text(response2.businesses[i].location.address1 + ", " + response2.businesses[i].location.city + ", " + response2.businesses[i].location.state + " " + response2.businesses[i].location.zip_code);
-                        var rating = $("<p class='card-text'>").text("Business is Rated: " + response2.businesses[i].rating);
-
-                        var popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
-                        popup.setLngLat({ lon: response2.businesses[i].coordinates.longitude, lat: response2.businesses[i].coordinates.latitude });
-                        popup.setHTML("<p>" + response2.businesses[i].name + "</p>")
-                        popup.setMaxWidth("300px")
-                        // popup.addTo(map);
-
-
-
-                        var marker = new mapboxgl.Marker({color: "purple" })
-                        marker.setLngLat([response2.businesses[i].coordinates.longitude, response2.businesses[i].coordinates.latitude])
-                        marker.addTo(map);
-                        marker.setPopup(popup);
-
-                        $(card).append(cardBody);
-                        $(cardBody).append(businessTitle);
-                        $(cardBody).append(phone);
-                        $(cardBody).append(address);
-                        $(cardBody).append(rating);
-                        $(card).css("width", "20%");
-                        $("#yelpHotel").append(card);
-                    }
-                }
-            }
-        });
-
-        $.ajax({
-            url: eventurl,
-            headers: {
-                'Authorization': 'Bearer BMqPeh9LSaDcy2Ynzrlg9b3Pg-zqx7NkPeU6d042djt_xHyFXUHMMftzKLTkw_ftN-_m3GRnBIxeyfvo_U9wHH1WRcYx0LsH6TMMMtyyqgp77l_raGrlydIVQUzBXHYx',
-            },
-            method: 'GET',
-            dataType: 'json',
-            success: function (getFuckedUp) {
-                console.log(getFuckedUp);
-                if (getFuckedUp.businesses.length > 0) {
-                    $("#eventsDiv").show();
-                    $("#events").empty();
-                    for (i = 0; i < 10; i++) {
-                        var card = $("<div class='card'>");
-                        var cardBody = $("<div class='card-body'>");
-                        var businessTitle = $("<a class='card-title'>").text(getFuckedUp.businesses[i].name);
-                        businessTitle.attr("href", getFuckedUp.businesses[i].url);
-                        var phone = $("<p class='card-subtitle mb-2 text-muted'>").text(getFuckedUp.businesses[i].phone);
-                        var address = $("<p class='card-text'>").text(getFuckedUp.businesses[i].location.address1 + ", " + getFuckedUp.businesses[i].location.city + ", " + getFuckedUp.businesses[i].location.state + " " + getFuckedUp.businesses[i].location.zip_code);
-                        var rating = $("<p class='card-text'>").text("Business is Rated: " + getFuckedUp.businesses[i].rating);
-
-                        var popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
-                        popup.setLngLat({ lon: getFuckedUp.businesses[i].coordinates.longitude, lat: getFuckedUp.businesses[i].coordinates.latitude });
-                        popup.setHTML("<p>" + getFuckedUp.businesses[i].name + "</p>")
-                        popup.setMaxWidth("300px")
-                        // popup.addTo(map);
-
-
-
-                        var marker = new mapboxgl.Marker({color: "yellow" })
-                        marker.setLngLat([getFuckedUp.businesses[i].coordinates.longitude, getFuckedUp.businesses[i].coordinates.latitude])
-                        marker.addTo(map);
-                        marker.setPopup(popup);
-
-                        $(card).append(cardBody);
-                        $(cardBody).append(businessTitle);
-                        $(cardBody).append(phone);
-                        $(cardBody).append(address);
-                        $(cardBody).append(rating);
-                        $(card).css("width", "20%");
-                        $("#events").append(card);
-                    }
-                }
-            }
-        })
-
-
         var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
         // Here we are building the URL we need to query the database
@@ -218,23 +80,26 @@ $(document).ready(function () {
                 // code to display weather on html here...
 
             });
-        //ZOMATO API//
-        //   api key: d26b1d8dcb1755149c3453df8f881735 //
+    });
 
-        // click function that's gonna trigger that call
+    locator.on("geolocate", function (e) {
+        
+        geoQuery = [e.coords.longitude, e.coords.latitude]
+        zomato(geoQuery);
+        yelp("restaurants", geoQuery, "#yelp", "#yelpDiv", "red");
+        yelp("hotels", geoQuery, "#yelpHotel", "#hotelDiv", "purple");
+        yelp("events", geoQuery, "#events", "#eventsDiv", "yellow");
 
-        event.preventDefault();
-        //grabbing value of the search input//
-        // Change to a geocode request and pass lon lat to the call and call the list of restaurants in the area. 
-        console.log("dude you're searching it up!");
-        // var zomatourl = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=" + zomatoQuery + "&apikey=d26b1d8dcb1755149c3453df8f881735"
-        var zomatourl = "https://developers.zomato.com/api/v2.1/geocode?lat=" + zomatoQuery[1] + "&lon=" + zomatoQuery[0] + "&apikey=d26b1d8dcb1755149c3453df8f881735"
+    });
+
+    function zomato(lngLat) {
+        var zomatourl = "https://developers.zomato.com/api/v2.1/geocode?lat=" + lngLat[1] + "&lon=" + lngLat[0] + "&apikey=d26b1d8dcb1755149c3453df8f881735"
         $.ajax({
             url: zomatourl,
             method: "GET",
             beforeSend: function (xhr) { xhr.setRequestHeader('user-key', 'd26b1d8dcb1755149c3453df8f881735'); },
         }).then(function (zomatoresponse) {
-            console.log(zomatoresponse);
+            
             // code to display to html here...
             if (zomatoresponse.nearby_restaurants.length > 0) {
                 $("#zomatoDiv").show();
@@ -248,6 +113,9 @@ $(document).ready(function () {
                     var address = $("<p class='card-text'>").text(zomatoresponse.nearby_restaurants[i].restaurant.location.address);
                     var rating = $("<p class='card-text'>").text("Business is Rated: " + zomatoresponse.nearby_restaurants[i].restaurant.user_rating.aggregate_rating);
                     var avgCost = $("<p class='card-text'>").text("Avg. Cost for Two: " + zomatoresponse.nearby_restaurants[i].restaurant.currency + zomatoresponse.nearby_restaurants[i].restaurant.average_cost_for_two);
+                    var coord = [zomatoresponse.nearby_restaurants[i].restaurant.location.longitude, zomatoresponse.nearby_restaurants[i].restaurant.location.latitude]
+                    marker(coord, "green", zomatoresponse.nearby_restaurants[i].restaurant.name)
+
                     $(card).append(cardBody);
                     $(cardBody).append(businessTitle);
                     $(cardBody).append(phone);
@@ -258,12 +126,67 @@ $(document).ready(function () {
                     $("#zomato").append(card);
                 }
             }
+        });
+    }
 
+    function yelp(type, yelpQuery, selector, divSelector, markerColor) {
+       var myurl = function url(yelpQuery){
+        if(yelpQuery === locationQuery){
+           return "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + type + "&location=" + yelpQuery;
+        }else{
+            return "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + type + "&longitude=" + yelpQuery[0] + "&latitude=" + yelpQuery[1];
+        }
+        } 
+        $.ajax({
+            url: myurl(yelpQuery),
+            headers: {
+                'Authorization': 'Bearer BMqPeh9LSaDcy2Ynzrlg9b3Pg-zqx7NkPeU6d042djt_xHyFXUHMMftzKLTkw_ftN-_m3GRnBIxeyfvo_U9wHH1WRcYx0LsH6TMMMtyyqgp77l_raGrlydIVQUzBXHYx',
+            },
+            method: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                
+                if (response.businesses.length > 0) {
+                    $(divSelector).show();
+                    $(selector).empty();
+                    for (i = 0; i < 10; i++) {
 
+                        var lonLat = [response.businesses[i].coordinates.longitude, response.businesses[i].coordinates.latitude]
+                        var card = $("<div class='card'>");
+                        var cardBody = $("<div class='card-body'>");
+                        var businessTitle = $("<a class='card-title'>").text(response.businesses[i].name);
+                        businessTitle.attr("href", response.businesses[i].url);
+                        var phone = $("<p class='card-subtitle mb-2 text-muted'>").text(response.businesses[i].phone);
+                        var address = $("<p class='card-text'>").text(response.businesses[i].location.address1 + ", " + response.businesses[i].location.city + ", " + response.businesses[i].location.state + " " + response.businesses[i].location.zip_code);
+                        var rating = $("<p class='card-text'>").text("Rating: " + response.businesses[i].rating);
+                        var price = $("<p class='card-text'>").text("Price: " + response.businesses[i].price);
 
+                        marker(lonLat, markerColor, response.businesses[i].name);
+
+                        $(card).append(cardBody);
+                        $(cardBody).append(businessTitle);
+                        $(cardBody).append(phone);
+                        $(cardBody).append(address);
+                        $(cardBody).append(rating);
+                        $(cardBody).append(price);
+                        $(card).css("width", "20%");
+                        $(selector).append(card);
+                    }
+                }
+            }
         });
 
-    });
+    };
 
+    function marker(coord, color, name) {
+        var popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
+        popup.setLngLat({ lon: coord[0], lat: coord[1] });
+        popup.setHTML("<p>" + name + "</p>")
+        popup.setMaxWidth("300px")
 
+        var marker = new mapboxgl.Marker({ color: color })
+        marker.setLngLat(coord)
+        marker.addTo(map);
+        marker.setPopup(popup);
+    }
 });
